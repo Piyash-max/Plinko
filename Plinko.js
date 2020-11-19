f0047e5d92b5ab995c267ca2d6bef81e11f47409
Piyash-max <71182127+Piyash-max@users.noreply.github.com>
2020-11-19 21:20:34 +0530

    Add files via upload

--- Plinko.js
+++ Plinko.js
@@ -1,14 +1,29 @@
 class Plinko {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
-    }
-    display(){
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
         
+        World.add(world, this.body);
     }
-}
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
